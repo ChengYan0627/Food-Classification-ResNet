48aed958e9d7d217acf01b75e5e075386385f774
TH-Austin-Chen <tienhaochen5115@gmail.com>
2025-12-10 13:02:12 -0800

    Lanczos, DPID, SAID

--- src/project_utils.py
+++ src/project_utils.py
@@ -5,20 +5,16 @@ import cv2
 from sklearn.metrics import f1_score
 warnings.filterwarnings("ignore", category=sklearn.exceptions.UndefinedMetricWarning)
 
+
 def calculate_metrics(true_labels: dict, pred_labels: dict, **kwargs):
-    """
-    Calculate accuracy and F1-score.
-    """
+    """Calculate accuracy and F1-score."""
     accuracy = sum(t == p for t, p in zip(true_labels, pred_labels)) / len(true_labels)
     f1 = f1_score(true_labels, pred_labels, average='weighted')
+    return {"accuracy": accuracy, "f1_score": f1}
 
-    return {"accuracy": accuracy, "f1_score": f1}   
 
 def preprocess_folder(function: callable, input_dir: str, output_dir: str):
-    """
-    Preprocess all the images in the input folder and save the results
-    """
-
+    """Preprocess all the images in the input folder and save the results"""
     os.makedirs(output_dir, exist_ok=True)
 
     for file_name in os.listdir(input_dir):
